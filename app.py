62f99eb7efec6bee4bcc4a59cc083959c9c9f9c5
Princy Razafimanantsoa <princy.raza@gmail.com>
2017-05-15 17:14:09 +0200

    login and access control

--- app.py
+++ app.py
@@ -3,71 +3,139 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 
-#Config MySQL
+# Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'myflaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-#init MYSQL
+# init MYSQL
 mysql = MySQL(app)
 Articles = Articles()
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/articles')
 def articles():
-  return render_template('articles.html', articles = Articles)
+    return render_template('articles.html', articles=Articles)
+
 
 @app.route('/article/<string:id>/')
 def article(id):
-  return render_template('article.html', id = id)
+    return render_template('article.html', id=id)
+
 
 class RegisterForm(Form):
-  name = StringField('Name', [validators.Length(min=1, max=50)])
-  username = StringField('Username', [validators.Length(min=4, max=25)])
-  email = StringField('Email', [validators.Length(min=6, max=50)])
-  password = PasswordField('Password', [
-    validators.DataRequired(),
-    validators.EqualTo('confirm', message='Passwords do not match')
-  ])
-  confirm = PasswordField('Confirm Password')
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-  form = RegisterForm(request.form)
-  if request.method == 'POST' and form.validate():
-    name = form.name.data
-    email = form.email.data
-    username = form.username.data
-    password = sha256_crypt.encrypt(str(form.password.data))
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
 
-    # create cursor
-    cur = mysql.connection.cursor()
+        # create cursor
+        cur = mysql.connection.cursor()
 
-    cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
-    
-    # commit to DB
-    mysql.connection.commit()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",
+                    (name, email, username, password))
 
-    # close connection
-    cur.close()
+        # commit to DB
+        mysql.connection.commit()
 
-    flash('You are now registered and can log in', 'success')
+        # close connection
+        cur.close()
 
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are logged out', 'success')
     return redirect(url_for('login'))
-  return render_template('register.html', form=form)
+
+# check if user logged in
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        flash('Unauthorized, please login', 'danger')
+        return redirect(url_for('login'))
+    return decorated_function
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # create cursor
+        cur = mysql.connection.cursor()
+
+        # get user by username
+        result = cur.execute('SELECT * FROM users WHERE username = %s', [username])
+
+        if result > 0:
+            # get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+
+            # close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
-  app.secret_key = 'secret123'
-  app.run(debug=True)
+    app.secret_key = 'secret123'
+    app.run(debug=True)
--- data.py
+++ data.py
@@ -2,24 +2,24 @@ def Articles():
     articles = [
         {
             'id': 1,
-            'title':'Article One',
-            'body':'Bespoke plaid taxidermy beard green juice actually vaporware slow-carb 3 wolf moon. Before they sold out wayfarers fap bitters, austin direct trade pork belly fingerstache truffaut vaporware scenester seitan hell of kickstarter. IPhone cray disrupt semiotics YOLO, readymade man braid copper mug pok pok succulents williamsburg salvia prism. Succulents fashion axe chillwave drinking vinegar, ugh paleo master cleanse. Godard vaporware ennui, pinterest tacos ramps brunch prism yr skateboard quinoa XOXO cronut slow-carb williamsburg. Gentrify kinfolk ethical yr. Readymade plaid listicle, keytar flannel intelligentsia affogato swag fashion axe fap truffaut +1 bitters migas.',
-            'author':'Princy',
-            'create_date':'04-25-2017'
+            'title': 'Article One',
+            'body': 'Bespoke plaid taxidermy beard green juice actually vaporware slow-carb 3 wolf moon. Before they sold out wayfarers fap bitters, austin direct trade pork belly fingerstache truffaut vaporware scenester seitan hell of kickstarter. IPhone cray disrupt semiotics YOLO, readymade man braid copper mug pok pok succulents williamsburg salvia prism. Succulents fashion axe chillwave drinking vinegar, ugh paleo master cleanse. Godard vaporware ennui, pinterest tacos ramps brunch prism yr skateboard quinoa XOXO cronut slow-carb williamsburg. Gentrify kinfolk ethical yr. Readymade plaid listicle, keytar flannel intelligentsia affogato swag fashion axe fap truffaut +1 bitters migas.',
+            'author': 'Princy',
+            'create_date': '04-25-2017'
         },
         {
             'id': 2,
-            'title':'Article Two',
-            'body':'Bespoke plaid taxidermy beard green juice actually vaporware slow-carb 3 wolf moon. Before they sold out wayfarers fap bitters, austin direct trade pork belly fingerstache truffaut vaporware scenester seitan hell of kickstarter. IPhone cray disrupt semiotics YOLO, readymade man braid copper mug pok pok succulents williamsburg salvia prism. Succulents fashion axe chillwave drinking vinegar, ugh paleo master cleanse. Godard vaporware ennui, pinterest tacos ramps brunch prism yr skateboard quinoa XOXO cronut slow-carb williamsburg. Gentrify kinfolk ethical yr. Readymade plaid listicle, keytar flannel intelligentsia affogato swag fashion axe fap truffaut +1 bitters migas.',
-            'author':'John Doe',
-            'create_date':'04-25-2017'
+            'title': 'Article Two',
+            'body': 'Bespoke plaid taxidermy beard green juice actually vaporware slow-carb 3 wolf moon. Before they sold out wayfarers fap bitters, austin direct trade pork belly fingerstache truffaut vaporware scenester seitan hell of kickstarter. IPhone cray disrupt semiotics YOLO, readymade man braid copper mug pok pok succulents williamsburg salvia prism. Succulents fashion axe chillwave drinking vinegar, ugh paleo master cleanse. Godard vaporware ennui, pinterest tacos ramps brunch prism yr skateboard quinoa XOXO cronut slow-carb williamsburg. Gentrify kinfolk ethical yr. Readymade plaid listicle, keytar flannel intelligentsia affogato swag fashion axe fap truffaut +1 bitters migas.',
+            'author': 'John Doe',
+            'create_date': '04-25-2017'
         },
         {
             'id': 3,
-            'title':'Article Three',
-            'body':'Bespoke plaid taxidermy beard green juice actually vaporware slow-carb 3 wolf moon. Before they sold out wayfarers fap bitters, austin direct trade pork belly fingerstache truffaut vaporware scenester seitan hell of kickstarter. IPhone cray disrupt semiotics YOLO, readymade man braid copper mug pok pok succulents williamsburg salvia prism. Succulents fashion axe chillwave drinking vinegar, ugh paleo master cleanse. Godard vaporware ennui, pinterest tacos ramps brunch prism yr skateboard quinoa XOXO cronut slow-carb williamsburg. Gentrify kinfolk ethical yr. Readymade plaid listicle, keytar flannel intelligentsia affogato swag fashion axe fap truffaut +1 bitters migas.',
-            'author':'Princy',
-            'create_date':'04-25-2017'
+            'title': 'Article Three',
+            'body': 'Bespoke plaid taxidermy beard green juice actually vaporware slow-carb 3 wolf moon. Before they sold out wayfarers fap bitters, austin direct trade pork belly fingerstache truffaut vaporware scenester seitan hell of kickstarter. IPhone cray disrupt semiotics YOLO, readymade man braid copper mug pok pok succulents williamsburg salvia prism. Succulents fashion axe chillwave drinking vinegar, ugh paleo master cleanse. Godard vaporware ennui, pinterest tacos ramps brunch prism yr skateboard quinoa XOXO cronut slow-carb williamsburg. Gentrify kinfolk ethical yr. Readymade plaid listicle, keytar flannel intelligentsia affogato swag fashion axe fap truffaut +1 bitters migas.',
+            'author': 'Princy',
+            'create_date': '04-25-2017'
         }
     ]
     return articles
